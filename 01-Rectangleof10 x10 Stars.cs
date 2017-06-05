bafbee179d21038730a76f96313fb19065743c18
platnoto <rgerdzhikov@gmail.com>
2017-06-05 14:29:41 +0300

    Modify 01-Rectangleof10 x10 Stars.cs. Just Test!

--- 01-Rectangleof10 x10 Stars.cs
+++ 01-Rectangleof10 x10 Stars.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+добавих пълна тъпотия (test somthing) 
 namespace _01_Rectangleof10_x10_Stars
 {
     class Program
